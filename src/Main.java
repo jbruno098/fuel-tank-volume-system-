457aa3a63b3395fc4a0870b7cf9c6551ea9aeb1b
J Bruno <jbrunorodrigues13@gmail.com>
2026-04-11 21:27:27 -0300

    implement CSV loading and tank volume lookup

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Map<Integer, Integer> table_tank_15000 = new HashMap<>();
+        Map<Integer, Integer> table_tank_7500 = new HashMap<>();
+
+        String table = "resources/tank_7500.csv";
+        String table2 =  "resources/tank_15000.csv";
+        loadTable(table2, table_tank_15000);
+        loadTable(table, table_tank_7500);
+
+        System.out.println("Teste 90cm (tanque 15.000L) -> " + table_tank_15000.get(90) + " litros");
+        System.out.println("teste 90cm (tanque 7500L -> " + table_tank_7500.get(90) + " litros");
+    }
+
+
+    public static void loadTable(String path, Map<Integer, Integer> map) {
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String linha;
+            br.readLine(); // pula primeira linha
+
+            while ((linha = br.readLine()) != null) {
+                String[] colunas = linha.split(",");
+                Integer cm = Integer.parseInt(colunas[0]);
+                Integer litros = Integer.parseInt(colunas[1]);
+                map.put(cm, litros);
+            }
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
