0b24f8ad84da6cb905c087f4ae5014a18b65616a
J Bruno <jbrunorodrigues13@gmail.com>
2026-04-20 16:19:18 -0300

    add entity MeasurementResult

---
+++ src/entity/MeasurementResult.java
@@ -0,0 +1,38 @@
+package entity;
+
+import java.time.LocalDateTime;
+
+public class MeasurementResult {
+
+    private Tank tank;
+    private Integer cm;
+    private Integer liters;
+    private LocalDateTime moment;
+
+    public MeasurementResult(Tank tank, Integer cm, Integer liters) {
+        this.tank = tank;
+        this.cm = cm;
+        this.liters = liters;
+        this.moment = LocalDateTime.now();
+    }
+
+    public Tank getTank() {
+        return tank;
+    }
+
+    public Integer getCm() {
+        return cm;
+    }
+
+    public Integer getLiters() {
+        return liters;
+    }
+
+    @Override
+    public String toString() {
+        return "Tanque: " + tank +
+                "\nCentímetros: " + cm +
+                "\nLitragem: " + liters +
+                "\nData/Hora: " + moment;
+    }
+}
--- src/services/TankService.java
+++ src/services/TankService.java
@@ -1,10 +1,24 @@
 package services;
 
+import entity.MeasurementResult;
 import entity.Tank;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TankService {
 
+    private List<MeasurementResult> results = new ArrayList<>();
+
     public Integer calculateLiters(Tank tank, Integer cm) {
-        return tank.getTable().searchLiters(cm);
+
+        Integer liters = tank.getTable().searchLiters(cm);
+        MeasurementResult result = new MeasurementResult(tank, cm, liters);
+        results.add(result);
+        return liters;
+    }
+
+    public List<MeasurementResult> getResults() {
+        return results;
     }
 }
