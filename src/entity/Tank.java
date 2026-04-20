a4b98b28415d637d3d0f3ee0bd8a0f46b9b9bc67
J Bruno <jbrunorodrigues13@gmail.com>
2026-04-20 09:08:15 -0300

    feat: add Tank id

--- src/entity/Tank.java
+++ src/entity/Tank.java
@@ -4,14 +4,20 @@ import entity.enums.FuelType;
 
 public class Tank {
 
+    private Long id;
     private Double capacity;
     private FuelType fuelType;
 
     public Tank() {}
 
-    public Tank(Double capacity, FuelType fuelType) {
+    public Tank(Long id, Double capacity, FuelType fuelType) {
+        this.id = id;
         this.capacity = capacity;
         this.fuelType = fuelType;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public Double getCapacity() {
